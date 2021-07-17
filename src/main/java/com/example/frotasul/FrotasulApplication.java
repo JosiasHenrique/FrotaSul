8c024b5157b9108dfedb625b489ea0c49d55509e
Josias Henrique <joystickhenrii@gmail.com>
2021-07-17 00:24:35 -0300

    Entidade motorista criada, mapeada, instanciada e testada

--- src/main/java/com/example/frotasul/FrotasulApplication.java
+++ src/main/java/com/example/frotasul/FrotasulApplication.java
@@ -1,13 +1,31 @@
 package com.example.frotasul;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.frotasul.entities.Motorista;
+import com.example.frotasul.repositories.MotoristaRepository;
+
 @SpringBootApplication
-public class FrotasulApplication {
+public class FrotasulApplication implements CommandLineRunner {
+	
+	@Autowired
+	private MotoristaRepository motoristaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrotasulApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Motorista mot1 = new Motorista(null, "Arquimedes", "412.457.587-77");
+		motoristaRepository.saveAll(Arrays.asList(mot1));
+		
 	}
 
 }
---
+++ src/main/java/com/example/frotasul/repositories/MotoristaRepository.java
@@ -0,0 +1,9 @@
+package com.example.frotasul.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.frotasul.entities.Motorista;
+
+public interface MotoristaRepository extends JpaRepository<Motorista, Long> {
+
+}
