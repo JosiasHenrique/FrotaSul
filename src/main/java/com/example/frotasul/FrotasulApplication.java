ff91c5d38fce59bf25065d1638700f4b6c1bbd76
Josias Henrique <joystickhenrii@gmail.com>
2021-07-17 00:04:09 -0300

    Projeto criado

---
+++ src/main/java/com/example/frotasul/FrotasulApplication.java
@@ -0,0 +1,13 @@
+package com.example.frotasul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrotasulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrotasulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/frotasul/FrotasulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frotasul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrotasulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
